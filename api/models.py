7e9f5f99a5a817e22b05e099d945802b37ac3259
david <dcarte51@msudenver.edu>
2025-01-29 21:36:35 -0700

    add basic models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    # You can still add custom fields as needed
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+
+    groups = models.ManyToManyField(
+       Group,
+       related_name='user_groups',
+       blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+       Permission,
+       related_name='user_permissions',
+       blank=True,
+    )
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.title}"
