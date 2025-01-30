0130a265a364c617ae9962a4fad494ddf7a5cb1c
david <dcarte51@msudenver.edu>
2025-01-29 22:16:37 -0700

    create sample user test

--- api/models.py
+++ api/models.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class User(AbstractUser):
-    # id = models.CharField(unique=True,)
-    # username wouldn't normally be PK, but for sake of time it is
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30, blank=True)
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+class UserTests(APITestCase):
+    def test_create_user(self):
+        url = reverse('users') # reverse looks up the view name
+        data = { # test data to create a user
+            'username': 'testuser',
+            'email': 'test@test.com',
+            'password': 'testpassword'
+        }
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['username'], data['username'])
+        self.assertEqual(response.data['email'], data['email'])
+        # ensure password isn't returned
+        self.assertNotIn('password', response.data)
